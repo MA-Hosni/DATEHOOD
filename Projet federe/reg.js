090421c1db49da32045f2c477ed77ce077ef4480
Mohamed Ali Hosni <124616606+MA-Hosni@users.noreply.github.com>
2023-04-06 15:27:14 +0100

    Add files via upload

---
+++ Projet federe/reg.js
@@ -0,0 +1,19 @@
+const registerButton = document.getElementById("register");
+const loginButton = document.getElementById("login");
+const container = document.getElementById("container");
+
+registerButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+loginButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
+
+
+// const loginBtn = document.getElementById("login-btn");
+
+// loginBtn.addEventListener("click", () => {
+//   window.location.href = 'file:///D:/Projet federe/index.html';
+// });
+

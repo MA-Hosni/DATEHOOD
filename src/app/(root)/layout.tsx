2b46189eff68fad0e3c06323dac09a91928eab50
mohamed ali hosni <mohamedalihosni25@gmail.com>
2024-07-02 21:08:07 +0100

    home and auth

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSideBar/LeftSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Datehood",
+  description: "A Social Media Platform Clone For Meta Threads Application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={inter.className}>
+          <Topbar />
+
+          <main className='flex flex-row'>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            {/* @ts-ignore */}
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ src/components/forms/AccountProfile.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+interface Props {
+    user: {
+      id: string;
+      objectId: string;
+      username: string;
+      name: string;
+      bio: string;
+      image: string;
+    };
+    btnTitle: string;
+  }
+
+const AccountProfile = ({ user, btnTitle }: Props) => {
+    return (
+        <div></div>
+    );
+};
+
+export default AccountProfile
